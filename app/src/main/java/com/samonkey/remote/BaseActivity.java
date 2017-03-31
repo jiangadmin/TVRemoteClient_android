8f76e4066433ef88f5da47977da86507921d6b94
jinghoulin <jinghoulin@samonkey.com>
2017-03-31 10:49:58 +0800

    1.着色状态栏 2.传统模式为app主入口

--- app/src/main/java/com/samonkey/remote/BaseActivity.java
+++ app/src/main/java/com/samonkey/remote/BaseActivity.java
@@ -1,6 +1,11 @@
 package com.samonkey.remote;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import com.samonkey.remote.utils.SystemBarTintManager;
 
 /**
  * Created on 2017/3/27
@@ -9,4 +14,16 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class BaseActivity extends AppCompatActivity {
+
+    private SystemBarTintManager mTintManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        // 透明状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        mTintManager = new SystemBarTintManager(this);
+        mTintManager.setStatusBarTintEnabled(true);
+        mTintManager.setStatusBarTintColor(getResources().getColor(R.color.statusColor));
+        super.onCreate(savedInstanceState);
+    }
 }
